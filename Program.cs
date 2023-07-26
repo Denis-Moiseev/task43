84454c4abdf2b611d778439f195a23c5b10c85fa
Denis-Moiseev <mrtesla666@gmail.com>
2023-07-26 21:53:59 +0800

    finish

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿void PointIntersectionTwoLines(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.Write($"({x}; {y})");
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double b1 = GetInput("Введите значение b1: ");
+double k1 = GetInput("Введите значение k1: ");
+double b2 = GetInput("Введите значение b2: ");
+double k2 = GetInput("Введите значение k2: ");
+Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ");
+PointIntersectionTwoLines(b1, k1, b2, k2);
